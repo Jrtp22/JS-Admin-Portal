ad904055e8d18511225a2ad01c9d9c707f376532
jrtp22 <jrtp22@gmail.com>
2023-03-19 14:57:09 -0700

    made titles appear

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,18 @@
 
-// Your Code Here
+async function getBooks(){
+let response = await fetch('http://localhost:3001/listBooks')
+let books = await response.json()
+
+books.forEach(renderBook)
+}
+function renderBook(book) {
+    console.log(book)
+    let bookContainer = document.getElementById('box')
+    bookContainer.innerHTML +=`
+       <ul>
+       <li>${book.title}</li>
+       
+       </ul>
+    `
+}
+getBooks()
